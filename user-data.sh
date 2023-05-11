3751cb05986c2f332a53df6853bbb861f8a8e5b2
Ritika <102703358+writetoritika@users.noreply.github.com>
2023-05-11 12:26:24 -0500

    Installed 2048 game (#9)

--- user-data.sh
+++ user-data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 yum update -y
-yum install -y httpd
+yum install -y httpd git
+git clone https://github.com/gabrielecirulli/2048.git
+cp -R 2048/* /var/www/html
 systemctl start httpd && systemctl enable httpd

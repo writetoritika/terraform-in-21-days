7c11d529dc5eac6ff311541a23798772210d9e95
Ritika <writetoritika@gmail.com>
2023-05-12 10:45:14 -0500

    Split terraform state into multiple layers

--- level1/provider.tf
+++ level1/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-remote-state-733796618401"
-    key            = "terraform.tfstate"
+    key            = "level1.tfstate"
     region         = "us-east-2"
     dynamodb_table = "terraform-remote-state"
   }

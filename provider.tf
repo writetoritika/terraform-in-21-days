b3434e500cc008076dea3fe7bf340adb68ef3ce5
Ritika <102703358+writetoritika@users.noreply.github.com>
2023-05-11 19:00:36 -0500

    Added remote state (#10)

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-remote-state-733796618401"
+    key            = "terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-remote-state"
+  }
+}
+
+
 provider "aws" {
   region = "us-east-2"
 }
+

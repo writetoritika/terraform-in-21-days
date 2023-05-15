2890dd5266178c0568edbcd5c65b463484114599
Ritika <102703358+writetoritika@users.noreply.github.com>
2023-05-15 10:23:57 -0500

    Configured load-balancer, security-group, target-group, others (#13)

--- level2/instance.tf
+++ level2/instance.tf
@@ -12,60 +12,15 @@ data "aws_ami" "amazonlinux" {
   }
 }
 
-resource "aws_instance" "public" {
-  ami                         = data.aws_ami.amazonlinux.id
-  associate_public_ip_address = true
-  instance_type               = "t3.micro"
-  key_name                    = "linuxkey"
-  vpc_security_group_ids      = [aws_security_group.public.id]
-  subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[0]
-  user_data                   = file("user-data.sh")
-  tags = {
-    Name = "${var.env_code}-public"
-  }
-}
-
-resource "aws_security_group" "public" {
-  name        = "${var.env_code}-public"
-  description = "Allow inbound traffic"
-  vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
-
-
-  ingress {
-    description = "SSH from public"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["73.32.30.227/32"]
-  }
-
-   ingress {
-    description = "HTTP from public"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.env_code}-public"
-  }
-}
-
 resource "aws_instance" "private" {
+  count                  =2
+
   ami                    = data.aws_ami.amazonlinux.id
   instance_type          = "t3.micro"
   key_name               = "linuxkey"
   vpc_security_group_ids = [aws_security_group.private.id]
-  subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[0]
-
+  subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[count.index]
+  user_data              = file("user-data.sh")
   tags = {
     Name = "${var.env_code}-private"
   }
@@ -83,6 +38,15 @@ resource "aws_security_group" "private" {
     protocol    = "tcp"
     cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr]
   }
+
+   ingress {
+    description    = "HTTP from load balancer"
+    from_port      = 80
+    to_port        = 80
+    protocol       = "tcp"
+    security_groups= [aws_security_group.load_balancer.id]
+  }
+ 
 
   egress {
     from_port   = 0
---
+++ level2/load-balancer.tf
@@ -0,0 +1,73 @@
+resource "aws_security_group" "load_balancer" {
+  name        = "${var.env_code}-load-balancer"
+  description = "Allow port 80 TCP inbound to ELB"
+  vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
+
+  ingress {
+    description = "http to ELB"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-load-balancer"
+  }
+}
+
+resource "aws_lb" "main" {
+  name               = var.env_code
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.load_balancer.id]
+  subnets            = data.terraform_remote_state.level1.outputs.public_subnet_id
+
+  tags = {
+    Name = var.env_code
+  }
+}
+
+resource "aws_lb_target_group" "main" {
+  name     = var.env_code
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.terraform_remote_state.level1.outputs.vpc_id
+
+  health_check {
+    enabled             = true
+    path                = "/"
+    port                = "traffic-port"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = 200
+  }
+}
+
+resource "aws_lb_target_group_attachment" "main" {
+  count = 2
+
+  target_group_arn = aws_lb_target_group.main.arn
+  target_id        = aws_instance.private[count.index].id
+  port             = 80
+}
+
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}

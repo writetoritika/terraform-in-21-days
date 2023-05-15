8e3dfbc99fcae97c80ee69eeab31163d1c510b88
Ritika <writetoritika@gmail.com>
2023-05-15 09:52:43 -0500

    Deleted additional ingress rule

--- level2/instance.tf
+++ level2/instance.tf
@@ -38,13 +38,7 @@ resource "aws_security_group" "private" {
     protocol    = "tcp"
     cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr]
   }
-  ingress {
-    description    = "HTTP from load balancer"
-    from_port      = 80
-    to_port        = 80
-    protocol       = "tcp"
-    security_groups= [aws_security_group.load_balancer.id]
-  }
+ 
 
   egress {
     from_port   = 0
